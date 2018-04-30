342ee471568f60479d91781edd9416aa1a26be5f
Daniel Rodriguez <daniel.rdriguez.rdriguez@gmail.com>
2018-04-30 14:56:26 +0200

    Set defaults correctly for method call without args

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
-name := "master_thesis-app"
+name := "thesisApp"
 
-version := "0.1"
+version := "1.0"
 
 scalaVersion := "2.11.8"
 
--- src/main/scala/thesisApp/ThesisAppLauncher.scala
+++ src/main/scala/thesisApp/ThesisAppLauncher.scala
@@ -17,10 +17,11 @@ object ThesisAppLauncher {
 
     val sourceFile = if (args.length > 0) args(0) else "./src/main/scala/data/DelightingCustomersBDextract2.json"
     val outputDir = if (args.length > 1) args(1) else "./src/main/scala/results/"
-    new File(outputDir).mkdirs()
-    val analisysList = if (args.length > 2) args.toList.drop(2) else List("./src/main/scala/data/dataExplorer_sample")
+    val analisysList = if (args.length > 2) args.toList.drop(2) else List("default")
 
     val timestamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd_HH.mm.ss"))
+    new File(outputDir).mkdirs()
+
     // Loading modules
     val utilities = new UtilsCarrefourDataset()
 
@@ -28,12 +29,11 @@ object ThesisAppLauncher {
     var timerModule = System.currentTimeMillis
     val formattedFile = utilities.fileFormatter(sourceFile, outputDir)
 
-
     // Starting SparkSession
     Logger.getLogger("org.apache.spark").setLevel(Level.WARN)
     val spark = SparkSession.builder
       .appName("TestAppLauncher")
-//      .master("local")
+            .master("local")
       .getOrCreate()
 
     // Loading formatted file as a dataframe table
@@ -46,9 +46,10 @@ object ThesisAppLauncher {
 
     for (analysisConf <- analisysList) {
 
+
       // Loading configuration from file or defaults
-      val source = scala.io.Source.fromFile(analysisConf)
-      val configJson = try JSON.parseFull(source.mkString) finally source.close()
+      val configJson = if (analysisConf == "default") { "" }
+      else { JSON.parseFull(scala.io.Source.fromFile(analysisConf).mkString) }
 
       val configMap: Map[String, String] = configJson match {
         case Some(e: Map[String, String] @unchecked) => e
@@ -86,73 +87,73 @@ object ThesisAppLauncher {
     }
 
   }
-    def dataExplorer(appName:String, df:DataFrame, resultsDir:String, utilities:UtilsCarrefourDataset): String = {
-      val explorer = new DataExploration()
-      explorer.dataExploration(appName, df, resultsDir, utilities)
-      appName
-    }
+  def dataExplorer(appName:String, df:DataFrame, resultsDir:String, utilities:UtilsCarrefourDataset): String = {
+    val explorer = new DataExploration()
+    explorer.dataExploration(appName, df, resultsDir, utilities)
+    appName
+  }
 
 
-    def recommenderALS(appName:String, configMap: Map[String, String], df:DataFrame, resultsDir:String, utilities:UtilsCarrefourDataset, clientConverter:IndexToString, productConverter:IndexToString): String = {
-      // Loading configuration from file or defaults
-      val usersCol = configMap.getOrElse("usersCol", "clientIndex")
-      val itemsCol = configMap.getOrElse("itemsCol", "prodNameIndex")
-      val ratingsCol = configMap.getOrElse("ratingsCol", "prodUds")
+  def recommenderALS(appName:String, configMap: Map[String, String], df:DataFrame, resultsDir:String, utilities:UtilsCarrefourDataset, clientConverter:IndexToString, productConverter:IndexToString): String = {
+    // Loading configuration from file or defaults
+    val usersCol = configMap.getOrElse("usersCol", "clientIndex")
+    val itemsCol = configMap.getOrElse("itemsCol", "prodNameIndex")
+    val ratingsCol = configMap.getOrElse("ratingsCol", "prodUds")
 
-      // Executing ALS analysis
-      val analizer = new AnalysisALS()
+    // Executing ALS analysis
+    val analizer = new AnalysisALS()
 
-      var (clientRecs, rmse) = analizer.analisysALS(
-        df.groupBy(itemsCol, usersCol)
-          .agg(functions.sum(ratingsCol).as(ratingsCol))
-      )
+    var (clientRecs, rmse) = analizer.analisysALS(
+      df.groupBy(itemsCol, usersCol)
+        .agg(functions.sum(ratingsCol).as(ratingsCol))
+    )
 
-      val cols = clientRecs.columns.toList
-      if (cols.contains("clientIndex")) { clientRecs = clientConverter.transform(clientRecs) }
-      if (cols.contains("prodNameIndex")) { clientRecs = productConverter.transform(clientRecs) }
-      val colDEIndexified = cols.map(x => x.replace("Index", ""))
+    val cols = clientRecs.columns.toList
+    if (cols.contains("clientIndex")) { clientRecs = clientConverter.transform(clientRecs) }
+    if (cols.contains("prodNameIndex")) { clientRecs = productConverter.transform(clientRecs) }
+    val colDEIndexified = cols.map(x => x.replace("Index", ""))
 
-      // Translate back elements and aggregate recommendations
-      val convertedClientRecs = clientRecs
-        .orderBy(desc(colDEIndexified(2)))
-        .groupBy(colDEIndexified(0))
-        .agg(collect_set(struct(colDEIndexified(1), colDEIndexified(2))).alias("recommendations"))
+    // Translate back elements and aggregate recommendations
+    val convertedClientRecs = clientRecs
+      .orderBy(desc(colDEIndexified(2)))
+      .groupBy(colDEIndexified(0))
+      .agg(collect_set(struct(colDEIndexified(1), colDEIndexified(2))).alias("recommendations"))
 
-      utilities.printFile(convertedClientRecs, resultsDir, appName + "(" + cols(0) + "," + cols(1) + ")_by_" + cols(2) + "_rmse_" + rmse)
-      appName + "(" + itemsCol + "," + usersCol + ")_rated_by_" + ratingsCol
-    }
-
-
-    def recommenderGraphD(appName:String, configMap: Map[String, String], df:DataFrame, resultsDir:String, utilities:UtilsCarrefourDataset): String = {
-      // Loading configuration from file or defaults
-      val vertexCol = configMap.getOrElse("vertexCol", "prodName")
-      val edgeCol = configMap.getOrElse("edgeCol", "clientIndex")
-
-      // Executing graph analysis
-      val graphAnalyzer = new AnalysisGraphD()
-      graphAnalyzer.analysisGraphD(
-        df.select(vertexCol, edgeCol),
-        resultsDir, utilities
-      )
-
-      appName + "(" + vertexCol + "," + edgeCol + ")"
-    }
+    utilities.printFile(convertedClientRecs, resultsDir, appName + "(" + cols(0) + "," + cols(1) + ")_by_" + cols(2) + "_rmse_" + rmse)
+    appName + "(" + itemsCol + "," + usersCol + ")_rated_by_" + ratingsCol
+  }
 
 
-    def recommenderGraphX(appName:String, configMap: Map[String, String], df:DataFrame, resultsDir:String, utilities:UtilsCarrefourDataset, spark:SparkSession): String = {
-      // Loading configuration from file or defaults
-      val vertexCol = configMap.getOrElse("vertexCol", "prodNameIndex")
-      val vertexPropertiesCol = configMap.getOrElse("vertexPropertiesCol", "prodName")
-      val edgeCol = configMap.getOrElse("edgeCol", "clientIndex")
+  def recommenderGraphD(appName:String, configMap: Map[String, String], df:DataFrame, resultsDir:String, utilities:UtilsCarrefourDataset): String = {
+    // Loading configuration from file or defaults
+    val vertexCol = configMap.getOrElse("vertexCol", "prodName")
+    val edgeCol = configMap.getOrElse("edgeCol", "clientIndex")
 
-      // Executing graph analysis
-      val graphAnalyzer = new AnalysisGraphX()
-      graphAnalyzer.analysisGraphX(
-        df.groupBy(vertexCol)
-          .agg(first(vertexPropertiesCol).as(vertexPropertiesCol)),
-        df.select(vertexCol, edgeCol),
-        resultsDir, utilities, spark
-      )
-      appName + "(" + vertexCol + "," + edgeCol + ")"
-    }
+    // Executing graph analysis
+    val graphAnalyzer = new AnalysisGraphD()
+    graphAnalyzer.analysisGraphD(
+      df.select(vertexCol, edgeCol),
+      resultsDir, utilities
+    )
+
+    appName + "(" + vertexCol + "," + edgeCol + ")"
+  }
+
+
+  def recommenderGraphX(appName:String, configMap: Map[String, String], df:DataFrame, resultsDir:String, utilities:UtilsCarrefourDataset, spark:SparkSession): String = {
+    // Loading configuration from file or defaults
+    val vertexCol = configMap.getOrElse("vertexCol", "prodNameIndex")
+    val vertexPropertiesCol = configMap.getOrElse("vertexPropertiesCol", "prodName")
+    val edgeCol = configMap.getOrElse("edgeCol", "clientIndex")
+
+    // Executing graph analysis
+    val graphAnalyzer = new AnalysisGraphX()
+    graphAnalyzer.analysisGraphX(
+      df.groupBy(vertexCol)
+        .agg(first(vertexPropertiesCol).as(vertexPropertiesCol)),
+      df.select(vertexCol, edgeCol),
+      resultsDir, utilities, spark
+    )
+    appName + "(" + vertexCol + "," + edgeCol + ")"
+  }
 }
